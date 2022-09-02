d1dc5700663e8b6d36e07b542bc0f691de5785d2
strongreasons <109900777+strongreasons@users.noreply.github.com>
2022-09-03 03:10:08 +0700

    ak3: update ak.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=TheOneMemory x ElectroWizard
+kernel.string=Join Telegram @ wzrdgrp
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
